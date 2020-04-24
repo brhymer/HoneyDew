ae918ab36ace1fdf063dc4b6ea118d9b5d4b564a
Brhymer <leiqien28@hotmail.com>
2020-04-24 14:18:57 -0700

    this commit adds Space.js

---
+++ models/Space.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const SpaceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    features: {
+      nestedObject: {
+        type: String,
+        // To Do: expand on what type of properties 'features' will have
+      },
+    },
+    // These properties still need fleshing out
+    Spaces: [],
+    Users: [],
+    Tasks: [],
+  },
+  { timestamps: true }
+);
+
+const Space = mongoose.model("Space", SpaceSchema);
+
+module.exports = Space;
