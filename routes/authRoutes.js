a97078377227600f3aa19cde797baf50506eae8d
minerupset <35759470+minerupset@users.noreply.github.com>
2020-04-24 15:00:49 -0700

    Basic login and register pages built

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,8 @@ router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
 
-router.get("/register", (req, res, next) => {});
+router.get("/register", (req, res, next) => {
+  res.render("auth/register");
+});
 
 module.exports = router;
