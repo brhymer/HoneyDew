33a2c29c25dc7e5b94c18026d6adb179ae897d11
minerupset <35759470+minerupset@users.noreply.github.com>
2020-04-27 16:07:57 -0700

    QC'd tasks so that it will work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,25 @@
 $(`#deleteTaskModal`).on("show.bs.modal", (event) => {
-  console.log("YOU CLICKED DELETE!");
+  const targetTask = event.relatedTarget;
+
+  document.querySelector(
+    "#delete-task-title"
+  ).innerHTML = targetTask.getAttribute("data-tasktitle");
+  const deleteForm = document.querySelector("#delete-task-form");
+  const targetTaskId = targetTask.getAttribute("data-taskid");
+  deleteForm.setAttribute("action", `/tasks/${targetTaskId}?_method=DELETE`);
+});
+
+$(`#showTaskModal`).on("show.bs.modal", (event) => {
+  const targetTask = event.relatedTarget;
+
+  console.log("You clicked show");
+  console.log(targetTask);
+  document.querySelector(
+    "#show-task-title"
+  ).innerHTML = targetTask.getAttribute("data-tasktitle");
+  const editButton = document.querySelector("#show-task-button");
+  const targetTaskId = targetTask.getAttribute("data-taskId");
+  editButton.setAttribute("href", `/tasks/edit/${targetTaskId}`);
 });
 
 function addFavIcon() {
