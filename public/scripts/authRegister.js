34e037f09af222b6a79509ef2427aae4ec046bd0
minerupset <35759470+minerupset@users.noreply.github.com>
2020-04-30 15:23:15 -0700

    Password validation in place

--- public/scripts/authRegister.js
+++ public/scripts/authRegister.js
@@ -1,6 +1,48 @@
+passwordEl = document.querySelector("#password");
 confirmPasswordEl = document.querySelector("#confirm-password");
+const registerButton = document.querySelector("#registerButton");
 
-confirmPasswordEl.addEventListener("blur", (event) => {
-  console.log(event.target);
-  console.log(document.querySelector("#password"));
+function flagPasswordError(messages) {
+  if (messages.length > 0) {
+    //Bad button
+    registerButton.disabled = true;
+    registerButton.style.cursor = "not-allowed";
+
+    //Show warning
+    messages.forEach((message) => {
+      const wrongPassword = document.createElement("p");
+      wrongPassword.classList.add("wrongPasswordWarning");
+      wrongPassword.style.color = "red";
+      wrongPassword.innerHTML = message;
+      document
+        .querySelector("#consent")
+        .insertAdjacentElement("beforebegin", wrongPassword);
+    });
+  }
+}
+
+confirmPasswordEl.addEventListener("blur", () => {
+  //Remove existing warnings
+  if (document.querySelectorAll(".wrongPasswordWarning")) {
+    const toDelete = document.querySelectorAll(".wrongPasswordWarning");
+    toDelete.forEach((elem) => elem.remove());
+  }
+  //Generate error messages
+  const errorMessages = [];
+  //Current rules:
+  //Passwords must match
+  //Length must be at least 12
+  if (passwordEl.value !== confirmPasswordEl.value) {
+    errorMessages.push("Passwords do not match");
+  }
+  if (passwordEl.value.length < 12) {
+    errorMessages.push("Password must be 12 characters or longer");
+  }
+
+  flagPasswordError(errorMessages);
+
+  if (errorMessages.length === 0) {
+    registerButton.disabled = false;
+    registerButton.style.cursor = "default";
+  }
 });
